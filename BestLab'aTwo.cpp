bb754e54b02f5b4b7937adec493b973adf783b69
Qargosh <85304615+Qargosh@users.noreply.github.com>
2021-06-03 20:20:17 +0300

    Create BestLab'aTwo.cpp

---
+++ BestLab'aTwo.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	char need;
+	cout << "Symbol: ";
+	cin >> need;
+	string inp;
+
+	cout << "Sring: ";
+	cin >> inp;
+	for (int i = 0; i < inp.length(); i++) {
+		if (inp[i] == need) {
+			cout << "Symbol num: " << i;
+			return 0;
+		}
+	}
+	cout << "No symbol";
+	return 0;
+}

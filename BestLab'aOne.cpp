3ba594aa7d30d01fe06750ec4cf2c0ac10d4bf4e
Qargosh <85304615+Qargosh@users.noreply.github.com>
2021-06-03 20:20:06 +0300

    Create BestLab'aOne.cpp

---
+++ BestLab'aOne.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+
+
+int main()
+{
+    int array[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    std::cout << "array:" << std::endl;
+    for (int i = 0; i < 10; i++) {
+        std::cout << array[i] << ' ';
+    }
+    std::cout << std::endl;
+    
+    for (int i = 0; i < 10; i++) {
+        if (array[i] % 2 == 0) {
+            array[i] = 0;
+        }
+    }
+
+
+    std::cout << "result array:" << std::endl;
+    for (int i = 0; i < 10; i++) {
+        std::cout << array[i] << ' ';
+    }
+    std::cout << std::endl;
+}
